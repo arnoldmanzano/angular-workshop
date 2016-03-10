6a969d45a19cc012ca53591649c76b440e2f033b
arnoldmanzano <arnold.manzano@gmail.com>
2016-03-10 10:23:43 +0000

    solved stage 2

--- app/js/controllers/counterController.js
+++ app/js/controllers/counterController.js
@@ -2,4 +2,26 @@ counterApp.controller("counterController", function(){
   var self = this;
 
   self.count = 0;
+
+  self.increment = function() {
+    self.count++;
+  };
+
+  self.decrement = function() {
+    self.count--;
+  };
+});
+
+
+counterApp.controller("repeatController", function(){
+  var self = this;
+  self.number = 2;
+  self.getArray = function() {
+    // return new Array(self.number);
+    var x = [];
+    for(var i=0; i<self.number; i++) {
+      x.push(i);
+    }
+    return x;
+  };
 });
--- test/e2e/counterSpec.js
+++ test/e2e/counterSpec.js
@@ -2,15 +2,40 @@ describe("Counter application", function(){
 
   it("Displays a counter", function(){
     browser.get("http://localhost:8000");
-    var counter = $("#count");
-    expect(counter.getText()).toBe("0");
+    var counter1 = $("#count1");
+    expect(counter1.getText()).toBe("0");
   });
 
-  it("Displays a counter", function(){
+  it("Clicking button increases count", function(){
     browser.get("http://localhost:8000");
-    var counter = $("#count");
-    var increment = $('#increment');
-    increment.click();
-    expect(counter.getText()).toBe("1");
+    var counter1 = $("#count1");
+    var increment1 = $('#increment1');
+    increment1.click();
+    expect(counter1.getText()).toBe("1");
+  });
+
+  it("Clicking button decreases count", function(){
+    browser.get("http://localhost:8000");
+    var counter1 = $("#count1");
+    var decrement1 = $('#decrement1');
+    decrement1.click();
+    expect(counter1.getText()).toBe("-1");
+  });
+
+  it("Displays two counters", function(){
+    browser.get("http://localhost:8000");
+    var counter1 = $("#count1");
+    var counter2 = $("#count2");
+    var increment1 = $('#increment1');
+    increment1.click();
+    expect(counter1.getText()).toBe("1");
+    expect(counter2.getText()).toBe("0");
+  });
+
+  it("Displays n counters", function(){
+    browser.get("http://localhost:8000");
+    var repeatnum = $("#repeatnum");
+    repeatnum.sendKeys('5');
+    expect(element(by.id('count5')).isPresent()).toBe(true);
   });
 });
